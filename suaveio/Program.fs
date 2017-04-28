bdf0cc996457e8773e258eb78f8d12ae6e9bb4c7
Lee Bryant <elvie.bryant@gmail.com>
2017-04-27 21:30:06 -0700

    Fired up a really simple web server in suave.

--- suaveio/Program.fs
+++ suaveio/Program.fs
@@ -1,6 +1,11 @@
 ﻿open System
+open Suave
+open Suave.Http
+
+let serverConfig = 
+  { defaultConfig with bindings = [HttpBinding.createSimple HTTP "127.0.0.1" 8070] }
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0
+  startWebServer serverConfig (Successful.OK "This is just the beginning!")
+  0
