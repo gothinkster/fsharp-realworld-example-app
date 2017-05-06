41dbe6e5d5e213f6e5d5b5251396c71f178e9fb7
Lee Bryant <elvie.bryant@gmail.com>
2017-05-05 23:21:25 -0700

    Added sample code for getting json from requests.
    Also added file for side effect operations.

--- suaveio/Program.fs
+++ suaveio/Program.fs
@@ -7,14 +7,22 @@ open RealWorld.Stubs
 open System.IO
 open Microsoft.Extensions.Configuration
 open MongoDB.Driver
+open RealWorld.Models
 
 let serverConfig = 
-  { defaultConfig with bindings = [HttpBinding.createSimple HTTP "127.0.0.1" 8070] }
+  { defaultConfig with bindings = [HttpBinding.createSimple HTTP "127.0.0.1" 8072] }
+
+let exampleGrabRequestJson () = 
+  request (fun inputGraph -> 
+    // This is how you get the requesting json from the ajax request
+    printfn "%A" (Suave.Json.fromJson<UserRequest> inputGraph.rawForm)
+    Successful.OK (sprintf "%A" inputGraph)
+  )
   
 // TODO: Replace each return comments with function to carry out the action.
 let app dbClient = 
   choose [
-    POST >=> path "/users/login" >=> (Successful.OK Responses.usersLogin)
+    POST >=> path "/users/login" >=> exampleGrabRequestJson ()
     POST >=> path "/users" >=> (Successful.OK Responses.usersLogin)
     GET  >=> path "/user" >=> (Successful.OK Responses.usersLogin)
     PUT  >=> path "/user" >=> (Successful.OK Responses.usersLogin)
@@ -36,7 +44,9 @@ let app dbClient =
     path "/" >=> (Successful.OK "This will return the base page.")
   ]
 
+open RealWorld.Effects.DB
+
 [<EntryPoint>]
-let main argv =
+let main argv = 
   startWebServer serverConfig (RealWorld.Effects.DB.getDBClient () |> app)
   0
--- suaveio/lib/Effects.fs
+++ suaveio/lib/Effects.fs
@@ -4,6 +4,11 @@ module DB =
   open Microsoft.Extensions.Configuration
   open MongoDB.Driver
   open System.IO
+  open RealWorld.Models 
+  open MongoDB.Bson
+  open MongoDB.Driver.Linq
+
+  // TODO: Convert side effects to return option types
 
   let getConfigDbConnection = 
     let builder = ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json")
@@ -12,4 +17,13 @@ module DB =
   let getDBClient () = 
     let mongoConn : string = getConfigDbConnection.GetValue("ConnectionStrings:DefaultConnection")
     let client = new MongoClient(mongoConn)
-    client.GetDatabase(getConfigDbConnection.GetValue("ConnectionStrings:dbname"))
+    client.GetDatabase(getConfigDbConnection.GetValue("ConnectionStrings:dbname")) 
+
+  let loginUser (request: UserRequest) (dbClient: IMongoDatabase) = 
+    let collection = dbClient.GetCollection<UserRequest>("Users")
+    let filter = Builders.Filter.Eq((fun doc -> doc.Password), request.Password)
+
+    // Return collections to avoid leaking nulls into your program from C#.
+    collection.Find(filter).ToList() |> Seq.first
+    
+    
