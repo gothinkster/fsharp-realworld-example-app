2f0d712ba062eae43702f642f1cd531b938cba13
Lee Bryant <elvie.bryant@gmail.com>
2017-06-06 21:50:26 -0700

    Added example integration test.

--- suaveio/tests/Integration.fs
+++ suaveio/tests/Integration.fs
@@ -5,7 +5,20 @@ open RealWorld.Effects.DB
 open MongoDB.Bson
 open MongoDB.Driver
 open MongoDB.Driver.Linq
+open Testing
+open Suave
+open Suave.Web
 
+(*
+  let GetRequest u =
+    let uri = new System.Uri("http://some.random.tld" + u)
+    let rawQuery = uri.Query.TrimStart('?')
+    let req = { HttpRequest.empty with url = uri ;
+                ``method`` = HttpMethod.GET ;
+                rawQuery = rawQuery }
+    { HttpContext.empty with request = req }
+*)
+let runWithDefaultConfig = runWith defaultConfig
 
 let databaseClient =
   let mongoConn : string = "mongodb://localhost:27017"
@@ -22,5 +35,13 @@ let tests =
     testCase "should blow up in your face" <| fun _ ->
       let result = RealWorld.Effects.Actions.getArticlesBy "something that's not there" databaseClient
       // TODO: Use suave testing for the correct response
+      printfn "Get articles result: %A" result
+
       Expect.equal true true "This string option from db"
+
+    testCase "Should return fake current user" <| fun _ -> 
+      // use data = new System.Net.Http.StringContent(sampleDocJson)
+      let result = runWithDefaultConfig (Program.app databaseClient) |> req HttpMethod.GET "/user" None
+      printfn "Request made to get the current user: %A" result   
+      Expect.equal true true "this will hit the real endpoint"
   ]
