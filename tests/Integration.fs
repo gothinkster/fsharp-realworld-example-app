d057c02283b5a857f2866e3fac93807221336eaf
Lee Bryant <elvie.bryant@gmail.com>
2017-07-29 10:34:56 -0700

    Test stub for empty collection.

--- tests/Integration.fs
+++ tests/Integration.fs
@@ -11,6 +11,7 @@ open MongoDB.Bson
 open MongoDB.Driver.Linq
 open MongoDB.Driver
 open RealWorld.Models
+  
 
 [<Tests>]
 let tests = 
@@ -18,5 +19,8 @@ let tests =
     testCase "Current working test for trying out impure functions" <| fun _ ->
       // let articles = RealWorld.Effects.DB.getSavedFollowedArticles databaseClient
       // Expect.equal articles None String.Empty
+      Expect.equal true true String.Empty
+
+    testCase "Create empty mongo collection" <| fun _ -> 
       Expect.equal true true String.Empty
   ]
