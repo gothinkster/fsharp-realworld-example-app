549faf3812814fe940aff56a088063a84d17a9f6
Lee Bryant <elvie.bryant@gmail.com>
2017-07-29 22:19:55 -0700

    Added unit test for extracting articles from option types.

--- tests/Articles.fs
+++ tests/Articles.fs
@@ -2,6 +2,7 @@ module Articles
 
 open Expecto
 open RealWorld.Convert
+open RealWorld.Models
 
 [<Tests>]
 let tests = 
@@ -9,4 +10,9 @@ let tests =
     testCase "should return default article" <| fun _ -> 
       let article = extractArticleList None 
       Expect.isEmpty article.article.title "did't work son"
+
+    testCase "Should return the actual article" <| fun _ ->
+      let article = { defaultArticle with article = { defaultArticle.article with title = "Model3"} }
+      let resultArticle = extractArticleList (Some article)
+      Expect.equal resultArticle.article.title "Model3" "Didn't get the same article back"
   ]
